db43c884f78352b1ab041df2b7e9553239696734
artog1809 <gdoq17@mail.ru>
2023-10-14 09:19:50 +0300

    Init commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const btnToggleTheme = document.querySelector('.btn-toggle-theme');
+const body = document.querySelector('body');
+btnToggleTheme.addEventListener('click', () => {
+  body.classList.toggle('dark-theme');
+  body.classList.toggle('light-theme');
+});
+
+const btnAsideMenu = document.querySelector('.btn-aside-menu');
+const asideMenu = document.querySelector('.aside-menu');
+btnAsideMenu.addEventListener('click', () => {
+  asideMenu.classList.add('show');
+});
+
+const btnCloseAsideMenu = document.querySelector('.aside-menu .close');
+btnCloseAsideMenu.addEventListener('click', () => {
+  asideMenu.classList.remove('show');
+});
