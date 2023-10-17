35b310a4b3dd8a2ab65cb1aed0d0cf544ba0a01c
artog1809 <gdoq17@mail.ru>
2023-10-18 00:22:47 +0300

    Added switcher for dark theme

--- script.js
+++ script.js
@@ -21,3 +21,10 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
 }
+
+const btnToggleTheme = document.querySelector('.btn-toggle-theme');
+const body = document.querySelector('body');
+btnToggleTheme.addEventListener('click', () => {
+  body.classList.toggle('dark-theme');
+  body.classList.toggle('light-theme');
+});
