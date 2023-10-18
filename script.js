73877baaa98241e22b5cfee89f0dbee6b96bbc97
artog1809 <gdoq17@mail.ru>
2023-10-18 10:32:59 +0300

    Added burger-menu

--- script.js
+++ script.js
@@ -26,3 +26,14 @@ btnToggleTheme.addEventListener('click', () => {
   body.classList.toggle('dark-theme');
   body.classList.toggle('light-theme');
 });
+
+const btnBurgerMenu = document.querySelector('.btn-burger-menu');
+const burgerMenu = document.querySelector('.burger-menu');
+btnBurgerMenu.addEventListener('click', () => {
+  burgerMenu.classList.add('show');
+});
+
+const btnCloseBurgerMenu = document.querySelector('.burger-menu .close');
+btnCloseBurgerMenu.addEventListener('click', () => {
+  burgerMenu.classList.remove('show');
+});
