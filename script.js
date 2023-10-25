9c3e65c2195d18223ef25446662729e5e491a8be
artog1809 <gdoq17@mail.ru>
2023-10-25 14:25:42 +0300

    Added accordion realization

--- script.js
+++ script.js
@@ -12,12 +12,12 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  slides[slideIndex-1].style.display = "block";
+  slides[slideIndex - 1].style.display = "block";
 }
 
 const btnToggleTheme = document.querySelector('.btn-toggle-theme');
@@ -37,3 +37,36 @@ const btnCloseBurgerMenu = document.querySelector('.burger-menu .close');
 btnCloseBurgerMenu.addEventListener('click', () => {
   burgerMenu.classList.remove('show');
 });
+
+const accordion = document.querySelector(".features-main-left");
+accordion.addEventListener('click', (event) => {
+  let btn = event.target.closest(".fml-block");
+  if (btn) {
+    const allBtns = document.querySelectorAll(".fml-block");
+    allBtns.forEach((elem, index) => {
+      if (elem === btn) {
+        elem.classList.add("active");
+        console.log(elem);
+        elem.querySelector(".firstblock-main").classList.add("show");
+        const imgs = [...document.querySelectorAll(".img-wrapper")];
+        const img = imgs.find((elem,i) => {
+          return i === index
+        })
+        if (img) { 
+          img.classList.add("show");
+        }
+      } else {
+        elem.classList.remove("active");
+        elem.querySelector(".firstblock-main").classList.remove("show");
+        const imgs = [...document.querySelectorAll(".img-wrapper")];
+        const img = imgs.find((elem,i) => {
+          return i === index
+        })
+        if (img) { 
+          img.classList.remove("show");
+        }
+      }
+    });
+  }
+})
+
